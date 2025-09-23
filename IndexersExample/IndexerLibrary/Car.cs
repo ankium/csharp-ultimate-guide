489195c1d18b0db6b939a299de5d583b26c44ebd
ankium <sudons@msn.cn>
2025-09-23 22:11:43 +0800

    Indexer Example

---
+++ IndexersExample/IndexerLibrary/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace IndexerLibrary
+{
+    public class Car
+    {
+        //private filed
+        private string[] _brands = new string[] { "BMW", "Skoda", "Honda" };
+        private string[] _names = new string[] { "first", "second", "third" };
+
+        //public indexer
+        public string this[int index]
+        {
+            set
+            {
+                this._brands[index] = value;
+            }
+            get
+            {
+                return this._brands[index];
+            }
+        }
+
+        //indexer overloading
+        public string this[string name]
+        {
+            set
+            {
+                this._brands[Array.IndexOf(_names, name)] = value;
+            }
+            get
+            {
+                return this._brands[Array.IndexOf(_names, name)];
+            }
+        }
+    }
+}
---
+++ IndexersExample/IndexersExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using IndexerLibrary;
+
+namespace IndexersExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of Car Class
+            Car car = new Car();
+
+            //call get accessor of indexer
+            System.Console.WriteLine(car[0]);//Output:BMW
+            System.Console.WriteLine(car["first"]);//Output:BMW
+
+            //call set accessor of indexer
+            car[0] = "Nissan";
+            System.Console.WriteLine(car[0]);
+
+            System.Console.ReadKey();
+        }
+    }
+}
