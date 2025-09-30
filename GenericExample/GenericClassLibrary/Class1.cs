2dd894b13780ac17d9b22b6e6d7831a9772e39be
ankium <sudons@msn.cn>
2025-09-30 09:34:24 +0800

    GenericExample

---
+++ GenericExample/GenericClassLibrary/Class1.cs
@@ -0,0 +1,65 @@
+﻿namespace GenericClassLibrary;
+
+public abstract class Student
+{
+    public abstract int Marks { get; set; }
+}
+
+public class GraduateStudent : Student
+{
+    public override int Marks { get; set; }
+}
+
+public class PostGraduateStudent : Student
+{
+    public override int Marks { get; set; }
+}
+
+public class Employee
+{
+    public int _salary;
+}
+
+public class Teacher
+{
+    public int _marks;
+}
+
+//generic class
+public class User<T>
+{
+    //generic field
+    public T _egistrationStatus;
+}
+
+//a class with generic method
+public class Sample
+{
+    public void PrintData<T>(T obj)
+    {
+        if (obj.GetType()==typeof(Employee))
+        {
+            Employee emp = obj as Employee;
+            System.Console.WriteLine(emp._salary);
+        }
+        else if (obj.GetType()==typeof(Teacher))
+        {
+            Teacher teach = obj as Teacher;
+            System.Console.WriteLine(teach._marks);
+        }
+        else
+        {
+            System.Console.WriteLine("invalid type");
+        }
+    }
+}
+//generic class with constraints (want to accept Student only)
+public class MarksPrinter<T> where T : Student
+{
+    public T _stu;
+    public void PrintMarks()
+    {
+        Student temp = (Student)_stu;
+        System.Console.WriteLine(temp.Marks);
+    }
+}
---
+++ GenericExample/GenericExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using GenericClassLibrary;
+
+namespace GenericExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create object of generic class
+        User<int> user1 = new User<int>();
+        user1._egistrationStatus = 1234;
+        System.Console.WriteLine(user1._egistrationStatus);
+        User<bool> user2 = new User<bool>();
+        user2._egistrationStatus = true;
+        System.Console.WriteLine(user2._egistrationStatus);
+        
+        //create object of generic class
+        Sample sample = new Sample();
+        Employee employee = new Employee(){_salary = 200};
+        Teacher teacher = new Teacher(){_marks = 80};
+        //call the generic method
+        sample.PrintData<Employee>(employee);
+        sample.PrintData<Teacher>(teacher);
+        
+        //create object of generic class
+        MarksPrinter<GraduateStudent> printer = new MarksPrinter<GraduateStudent>();
+        printer._stu = new GraduateStudent() { Marks = 90 };
+        printer.PrintMarks();
+        
+        System.Console.ReadKey();
+    }
+}
