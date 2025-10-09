9fdc826f7dd15721f82f9e7a54b8486406f74a6c
ankium <sudons@msn.cn>
2025-10-09 12:55:28 +0800

    Events Example

---
+++ EventsExample/ClassLibraryExample/Publisher.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace ClassLibraryExample
+{
+    //事件参数
+    public class CustomEventArgs : EventArgs
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+    }
+    public class Publisher
+    {
+        //创建事件
+        public event EventHandler<CustomEventArgs> CustomEvent;
+        //触发事件
+        public void RaiseEvent(object sender,int a, int b)
+        {
+            if (this.CustomEvent != null)
+            {
+                CustomEventArgs e = new CustomEventArgs() { num1 = a, num2 = b };
+                this.CustomEvent(this, e);
+            }
+        }
+    }
+}
---
+++ EventsExample/EventsExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibraryExample;
+using System.Runtime.CompilerServices;
+
+namespace EventsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.DoWork();
+        }
+
+        private void DoWork()
+        {
+            Publisher publisher = new Publisher();
+            //订阅事件
+            publisher.CustomEvent += (sender, e) =>
+            {
+                int c = e.num1 + e.num2;
+                Console.WriteLine(c);
+            };
+            //调用事件
+            publisher.RaiseEvent(this, 20, 15);
+            Console.ReadKey();
+        }
+    }
+}
