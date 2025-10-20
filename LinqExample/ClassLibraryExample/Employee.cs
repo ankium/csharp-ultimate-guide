c3fa0f36def1cf7baeae776f2795b7885df34fa1
ankium <sudons@msn.cn>
2025-10-20 22:05:54 +0800

    Linq Example

---
+++ LinqExample/ClassLibraryExample/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibraryExample;
+
+public class Employee
+{
+    public int EmpId { get; set; }
+    public string EmpName { get; set; }
+    public string Job { get; set; }
+    public double Salary { get; set; }
+}
---
+++ LinqExample/LinqExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibraryExample;
+
+namespace LinqExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee() { EmpId = 101, EmpName = "Henry", Job = "Designer", Salary = 7232 },
+            new Employee() { EmpId = 102, EmpName = "Jack", Job = "Developer", Salary = 4500 },
+            new Employee() { EmpId = 103, EmpName = "Gabriel", Job = "Analyst", Salary = 1293 },
+            new Employee() { EmpId = 104, EmpName = "William", Job = "Manager", Salary = 2873 },
+            new Employee() { EmpId = 105, EmpName = "Alexa", Job = "Manager", Salary = 6232 }
+        };
+        List<Employee> newEmployeeList = employees.OrderBy(emp => emp.EmpName).ToList();
+        foreach (var emp in newEmployeeList)
+        {
+            Console.WriteLine(emp.EmpId+","+emp.EmpName+","+emp.Job+","+emp.Salary);
+        }
+    }
+}
