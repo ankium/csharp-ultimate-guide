488c07e728555304dda30cc4a2a8f0b4e6761c4b
ankium <sudons@msn.cn>
2025-11-09 22:04:57 +0800

    添加配置设置

---
+++ HarshaBank/HarshaBank.Configuration/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace HarshaBank.Configuration;
+
+/// <summary>
+/// Customer number starts from 1001;incremented by 1;
+/// </summary>
+public static class Settings
+{
+    public static long BaseCustomerNo { get; set; } = 1000;
+}
