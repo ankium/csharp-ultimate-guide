5389b91b25c3276b439582d80847626a249e9e75
ankium <sudons@msn.cn>
2026-01-19 15:41:23 +0800

    Some properties of Thread class

--- ThreadingApp/ThreadingApp/Program.cs
+++ ThreadingApp/ThreadingApp/Program.cs
@@ -5,14 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Thread mainThread = Thread.CurrentThread;
+        // Some properties of Thread class
         mainThread.Name = "Main-Thread"; 
-        System.Console.WriteLine(mainThread.ToString());
         System.Console.WriteLine(mainThread.Name);
-        Method1();
+        System.Console.WriteLine(mainThread.Priority);
+        System.Console.WriteLine(mainThread.IsAlive);
+        System.Console.WriteLine(mainThread.IsBackground);
+        System.Console.WriteLine(mainThread.ThreadState);
         System.Console.ReadKey();
-    }
-    static void Method1()
-    {
-        System.Console.WriteLine("Method-1");
     }
 }
