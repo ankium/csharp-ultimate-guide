846709942fc720ac4e9af1e70e263454b58701db
ankium <sudons@msn.cn>
2026-01-19 13:07:59 +0800

    ThreadingApp

---
+++ ThreadingApp/ThreadingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ThreadingApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread mainThread = Thread.CurrentThread;
+        mainThread.Name = "Main-Thread"; 
+        System.Console.WriteLine(mainThread.ToString());
+        System.Console.WriteLine(mainThread.Name);
+        Method1();
+        System.Console.ReadKey();
+    }
+    static void Method1()
+    {
+        System.Console.WriteLine("Method-1");
+    }
+}
