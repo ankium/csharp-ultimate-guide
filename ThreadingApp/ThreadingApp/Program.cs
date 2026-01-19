30feacc6df8ad8943c4aef439ab35d8ff2b0358a
ankium <sudons@msn.cn>
2026-01-19 16:09:40 +0800

    Single Threaded App

--- ThreadingApp/ThreadingApp/Program.cs
+++ ThreadingApp/ThreadingApp/Program.cs
@@ -1,17 +1,48 @@
-﻿namespace ThreadingApp;
+﻿using System.Reflection.PortableExecutable;
+
+namespace ThreadingApp;
+
+class NumbersCounter
+{
+    public void CountUp()
+    {
+        // i = 1 to 100
+        for (int i = 1; i <= 100; i++)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.Write($"i={i}, ");
+        }
+    }
+
+    public void CountDown()
+    {
+        // j = 100 to 1
+        for (int j = 100; j >= 1; j--)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.Write($"j={j}, ");
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Get main thread
         Thread mainThread = Thread.CurrentThread;
-        // Some properties of Thread class
         mainThread.Name = "Main-Thread"; 
-        System.Console.WriteLine(mainThread.Name);
-        System.Console.WriteLine(mainThread.Priority);
-        System.Console.WriteLine(mainThread.IsAlive);
-        System.Console.WriteLine(mainThread.IsBackground);
-        System.Console.WriteLine(mainThread.ThreadState);
-        System.Console.ReadKey();
+        System.Console.WriteLine(mainThread.Name); // Main thread
+
+        // Create a NumbersCounter instance
+        NumbersCounter counter = new NumbersCounter();
+        // Invoke CountUp
+        counter.CountUp();
+
+        //invoke CountDown
+        counter.CountDown();
+
+        System.Console.WriteLine(mainThread.Name+" has finished execution."); 
+        //System.Console.ReadKey();
     }
 }
