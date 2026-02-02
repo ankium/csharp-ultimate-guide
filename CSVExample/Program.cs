f9cdc0d133f3d9b0731573a97075ea3fad868e64
ankium <sudons@msn.cn>
2026-02-02 22:09:13 +0800

    Create CSVExample Project

---
+++ CSVExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using CSVExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region DataCSV
+        File.WriteAllText(Shared.FilePath,"");
+        #endregion
+
+        //Read CSV file in chunks and process each chunk in parallel
+        using(StreamReader sr = new StreamReader(Shared.FilePath))
+        {
+            string? line;
+            int lineNumber = 0;
+
+            //Create a list to hold the current chunk
+            List<string> chunk = new List<string>();
+
+            //Read each line from the CSV file
+            while((line = sr.ReadLine()) != null)
+            {
+                //Skip the iteration if the line is empty
+                if(string.IsNullOrEmpty(line))
+                    continue;
+                //Line is not empty, add it to the chunk and increment line number
+                lineNumber++;
+                chunk.Add(line);
+                //If chunk size is reached, process the chunk in parallel
+                if(lineNumber % Shared.ChunkSize == 0)
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ CSVExample/Shared.cs
@@ -0,0 +1,15 @@
+﻿namespace CSVExample;
+
+public class Shared
+{
+    public static object LockObject { get; set; }
+    public static string FilePath { get; set; }
+    public static int ChunkSize { get; set; }
+
+    public Shared()
+    {
+        LockObject = new object();
+        FilePath = "data.csv";
+        ChunkSize = 100;
+    }
+}
