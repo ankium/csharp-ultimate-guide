d5b44398fedca433b45cbdedee824cb1bab7f212
ankium <sudons@msn.cn>
2026-02-11 14:51:26 +0800

    泛型Task

--- TaskExample/Program.cs
+++ TaskExample/Program.cs
@@ -4,27 +4,38 @@ namespace TaskExample;
 
 class UpCounter
 {
-    public void CountUp(int count)
+    public long CountUp(int count)
     {
+        long sum = 0;
         System.Console.WriteLine("\nCount-Up Start.");
         for (int i = 1; i < count; i++)
         {
             System.Console.Write($"i={i}, ");
+            sum+=i;
         }
         System.Console.WriteLine("\nCount-Down Finished.");
+        return sum;
     }
 }
 class DownCounter
 {
-    public void CountDown(int count)
+    public SumData CountDown(int count)
     {
+        long sum = 0;
         System.Console.WriteLine("\nCount-Down Start.");
         for (int j = count; j >= 1; j--)
         {
             System.Console.Write($"j={j}, ");
+            sum+=j;
         }
         System.Console.WriteLine("\nCount-Down Finisied.");
+        return new SumData(){Sum = sum};
     }
+}
+
+class SumData
+{
+    public long Sum { get; set; }
 }
 class Program
 {
@@ -43,18 +54,20 @@ class Program
         UpCounter upCounter = new UpCounter();
         DownCounter downCounter = new DownCounter();
 
-        Task task1 = Task.Factory.StartNew(() =>
+        Task<long> task1 = Task.Run(() =>
         {
-            upCounter.CountUp(20);
+            return upCounter.CountUp(20);
         });
 
-        Task task2 = Task.Factory.StartNew(() =>
+        Task<SumData> task2 = Task.Factory.StartNew(() =>
         {
-            downCounter.CountDown(25);
+            return downCounter.CountDown(25);
         });
 
         // task1.Wait();
         // task2.Wait();
         Task.WaitAll(task1,task2);
+        System.Console.WriteLine($"Result from Count-Up: {task1.Result}");
+        System.Console.WriteLine($"Result from Count-Down: {task2.Result.Sum}");
     }
 }
