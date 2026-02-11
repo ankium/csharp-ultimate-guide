95ccc83499e5f020161d1cc168d64f0e23fa7b79
ankium <sudons@msn.cn>
2026-02-11 13:41:21 +0800

    Stopwatch

--- TaskExample/Program.cs
+++ TaskExample/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace TaskExample;
+﻿using System.Diagnostics.Metrics;
+
+namespace TaskExample;
+
 class UpCounter
 {
     public void CountUp(int count)
@@ -16,7 +19,7 @@ class DownCounter
     public void CountDown(int count)
     {
         System.Console.WriteLine("\nCount-Down Start.");
-        for (int j = count; j >=1; j--)
+        for (int j = count; j >= 1; j--)
         {
             System.Console.Write($"j={j}, ");
         }
@@ -27,17 +30,74 @@ class Program
 {
     static void Main(string[] args)
     {
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        WithTask();
+        stopwatch.Stop();
+        long timeTakenForTasks = stopwatch.ElapsedMilliseconds;
+        System.Console.WriteLine($"\nTasks - Time Taken: {timeTakenForTasks} ms");
+
+        stopwatch.Restart();
+        WithThreads();
+        stopwatch.Stop();
+        long timeTakenForThreads = stopwatch.ElapsedMilliseconds;
+        System.Console.WriteLine($"\nThreads - Time Taken: {timeTakenForThreads} ms");
+
+        if (timeTakenForTasks < timeTakenForThreads)
+        {
+            System.Console.WriteLine("TPL is faster.");
+        }
+        else if (timeTakenForTasks > timeTakenForThreads)
+        {
+            System.Console.WriteLine("Threads is faster.");
+        }
+        else
+        {
+            System.Console.WriteLine("Both TPL and Threads is equal.");
+        }
+    }
+
+    static void WithTask()
+    {
         UpCounter upCounter = new UpCounter();
         DownCounter downCounter = new DownCounter();
+        CountdownEvent countdownEvent = new CountdownEvent(2);
 
         Task task1 = Task.Run(() =>
         {
-           upCounter.CountUp(20); 
+            upCounter.CountUp(20);
+            countdownEvent.Signal();
         });
 
         Task task2 = Task.Run(() =>
         {
             downCounter.CountDown(25);
+            countdownEvent.Signal();
         });
+
+        countdownEvent.Wait();
+    }
+
+    static void WithThreads()
+    {
+        UpCounter upCounter = new UpCounter();
+        DownCounter downCounter = new DownCounter();
+        CountdownEvent countdownEvent = new CountdownEvent(2);
+
+        Thread thread1 = new Thread(() =>
+        {
+            upCounter.CountUp(20);
+            countdownEvent.Signal();
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            downCounter.CountDown(25);
+            countdownEvent.Signal();
+        });
+
+        thread1.Start();
+        thread2.Start();
+        countdownEvent.Wait();
     }
 }
